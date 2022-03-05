9ac8f2a70c1e58aac9b582a271a1e34bd21d6a1f
Kat Camp <kcamp552369@gmail.com>
2022-03-05 18:31:36 -0500

    initial setup

--- src/main/java/com/careerdevs/HelloInternet/controllers/RootController.java
+++ src/main/java/com/careerdevs/HelloInternet/controllers/RootController.java
@@ -1,4 +1,11 @@
 package com.careerdevs.HelloInternet.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;#RestController
 public class RootController {
+
+     @GetMapping ("/")
+
+    private String  rootRoute(){
+        return "you requested the route code";
+    }
 }
